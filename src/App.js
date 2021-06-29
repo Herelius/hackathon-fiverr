cae57486d4158720290faba671bbaff1bc01364e
BuG-Dev-Joris <maupied69@hotmail.com>
2021-06-29 16:08:15 +0200

    added ui navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import SelectPage from './assets/SelectPage';
+import PrimarySearchAppBar from './components/navbar';
 
 const App = () => {
   return (
     <div className="App">
+      <PrimarySearchAppBar />
       <SelectPage />
     </div>
   );
